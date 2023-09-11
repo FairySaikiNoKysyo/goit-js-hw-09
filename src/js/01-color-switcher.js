4e28eb73fda4564706331a1fd9838ce19b451de5
FairySaikiNoKysyo <michaelevgeniymyers@gmail.com>
2023-09-11 18:50:27 +0300

    add

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,27 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const body = document.body;
+
+startBtn.addEventListener('click', onClickStart);
+stopBtn.addEventListener('click', onClickStop);
+stopBtn.disabled = true;
+let interval = 0;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+};
+
+function onClickStart() {
+    startBtn.disabled = true;
+    interval = setInterval(() => {
+        body.style.backgroundColor = getRandomHexColor();
+        
+        stopBtn.disabled = false;
+    }, 1000);
+};
+
+function onClickStop() {    
+    interval = clearInterval(interval);
+    startBtn.disabled = false;  
+    stopBtn.disabled = true;
+};
